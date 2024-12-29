9861d089ea15c04a5ba9cb519877d59239a7de00
Mikkel Bjoern <mb@halfspace.io>
2024-12-29 23:55:41 +0100

    added use

--- 2019/src/solutions.rs
+++ 2019/src/solutions.rs
@@ -1,6 +1,8 @@
+use std::fmt::Display;
+
 pub mod day01;
 
-pub struct SolutionSet<O1: std::fmt::Display, O2: std::fmt::Display> {
+pub struct SolutionSet<O1: Display, O2: Display> {
     pub part1: Option<O1>,
     pub part2: Option<O2>,
 }
@@ -14,8 +16,8 @@ pub fn get_solver(day: i32) -> impl Solver {
 
 pub trait Solver {
     type I;
-    type O1: std::fmt::Display;
-    type O2: std::fmt::Display;
+    type O1: Display;
+    type O2: Display;
 
     fn parse_input(&self, input: &str) -> Self::I;
     fn part_1(&self, input: &Self::I) -> Option<Self::O1>;
